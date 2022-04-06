26109741cf63a9ea8084e048ee52726873111797
Mendel Aguilar <mendelaguilar1999123@gmail.com>
2022-04-06 02:34:21 -0600

    Data Table PayForm, Post en desarrollo

---
+++ src/components/payForm/AddPayForm.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import axios from "../../config/axios";
+import token from "../../helpers/getToken";
+
+const AddPayForm = () => {
+    const [formAddPayForm, setFormAddPayForm] = useState({
+        FIRST_NAME: '',
+        LAST_NAME: '',
+        HOURS_WORKED: '',
+        AMO_GROSS: '',
+        BONUS: '',
+        TOT_DEDUCTIONS: '',
+        NET_SALARY: '',
+        DAT_PAYMENT: ''
+    });
+
+    const handleInputChange = (e) => {
+        setFormAddPayForm({
+            ...formAddPayForm,
+            [e.target.name]: e.target.value
+        })
+    } 
+
+    const handleSubmitPayForm = (e) => {
+        e.preventDefault();
+        axios.post('/pay-form', formAddPayForm, token)
+           .then(res => console.log(res))
+    }
+
+
+    return(
+        <form id='addPayForm' onSubmit={handleSubmitPayForm} action='#'>
+        <div className="row mb-4">
+            <div className="col-md-6">
+                <label className='form-label' htmlFor="FIRST_NAME">Nombre</label>
+                <input onChange={handleInputChange} className='form-control' name='FIRST_NAME' type="text" required/>
+            </div>
+            <div className="col-md-6">
+                <label className='form-label' htmlFor="LAST_NAME">Apellido</label>
+                <input onChange={handleInputChange} className='form-control' name='LAST_NAME' type="text" required/>
+            </div>
+            <div className="col-md-3">
+                <label className='form-label' htmlFor="HOURS_WORKED">Horas Trabajadas</label>
+                <input onChange={handleInputChange} className='form-control' name='HOURS_WORKED' type="number" required/>
+            </div>
+            <div className="col-md-3">
+                <label className='form-label' htmlFor="AMO_GROSS">Salario Base</label>
+                <input onChange={handleInputChange} className='form-control' name='AMO_GROSS' type="number" required/>
+            </div>
+            <div className="col-md-3">
+                <label className='form-label' htmlFor="BONUS">Bonificaciones</label>
+                <input onChange={handleInputChange} className='form-control' name='BONUS' type="number" required/>
+            </div>
+            <div className="col-md-3">
+                <label className='form-label' htmlFor="TOT_DEDUCTIONS">Deducciones</label>
+                <input className='form-control' name='TOT_DEDUCTIONS' type="number" required/>
+            </div>
+            <div className="col-md-3">
+                <label className='form-label' htmlFor="NET_SALARY">Salario Neto</label>
+                <input onChange={handleInputChange} className='form-control' name='NET_SALARY' type="number" required/>
+            </div>
+            <div className="col-md-4">
+                <label className='form-label' htmlFor="DAT_PAYMENT">Fecha de Pago</label>
+                <input onChange={handleInputChange} className='form-control' name='DAT_PAYMENT' type="date" required/>
+            </div>
+        </div>
+        <div className="modal-footer">
+            <button type="button" id='idClosePayForm' className="btn btn-primary" data-dismiss="modal">Cerrar</button>
+            <button type='submit' className="btn btn-success">Guardar</button>
+        </div>
+    </form>
+    )
+}
+
+export default AddPayForm;
--- src/pages/Dashboard/Contabilidad/PlanillaPago.jsx
+++ src/pages/Dashboard/Contabilidad/PlanillaPago.jsx
@@ -1,8 +1,137 @@
+import DataTable from 'react-data-table-component';
+
+import {useEffect, useState, useMemo} from 'react';
+
+import Spinner from '../../../components/Spinner';
+import FilterComponent from '../../../components/FilterComponent';
+import Modal from '../../../components/Modal';
+import AddPayForm from '../../../components/payForm/AddPayForm';
+
+import {paginationComponentOptions} from '../../../helpers/datatablesOptions';
+import axios from '../../../config/axios'
+import token from '../../../helpers/getToken';
+
 
 const PlanillaPago = () => {
+    const [rows, setRows] = useState([]);
+    const [filterText, setFilterText] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [messageError, setMessageError] = useState('');
+    
+    //definir las columnas
+    const columns = [
+        {
+            name: 'CODIGO',
+            selector: row => row.COD_PAY_FORM,
+            sortable: true,
+        },
+        {
+            name: 'NOMBRE',
+            selector: row => row.FIRST_NAME,
+            sortable: true,
+        },
+        {
+            name: 'APELLIDO',
+            selector: row => row.LAST_NAME,
+            sortable: true,
+        },
+        {
+            name: 'HORAS TRABAJADAS',
+            selector: row => row.HOURS_WORKED,
+            sortable: true,
+        },
+        {
+            name: 'SALARIO BASE',
+            selector: row => row.AMO_GROSS,
+            sortable: true,
+        },
+        {
+            name: 'BONIFICACIONES',
+            selector: row => row.BONUS,
+            sortable: true,
+        },
+        {
+            name: 'DEDUCCIONES',
+            selector: row => row.TOT_DEDUCTIONS,
+            sortable: true,
+        },
+        {
+            name: 'SALARIO NETO',
+            selector: row => row.NET_SALARY,
+            sortable: true,
+        },
+        {
+            name: 'FECHA DE PAGO',
+            selector: row => row.DAT_PAYMENT,
+            sortable: true,
+        },
+        {
+            name: 'ACCIONES',
+            button: true,
+            cell: row => <>
+                <button className='btn btn-sm btn-warning mr-1' data-toggle="modal" data-target='#'><i className="fa-solid fa-pen-to-square"></i></button>
+                <button className='btn btn-sm btn-danger'><i className="fa-solid fa-trash"></i></button>
+            </>
+        }
+    ];
+
+    //datos
+    const filteredItems = rows.filter(item => JSON.stringify(item).toLowerCase().indexOf(filterText.toLowerCase()) !== -1);
+
+    //Componente del buscador
+    const subHeaderComponentMemo = useMemo(() => {
+    	return (
+        		<FilterComponent onFilter={e => setFilterText(e.target.value)} filterText={filterText} />
+        	);
+    }, [filterText]);
+
+    useEffect(() => {
+        //PETICION GET
+        axios.get('/pay-form', token)
+            .then(res => {
+                const {data} = res;
+                console.log(data);
+                setRows(data);
+                setLoading(false);
+            })
+    },[]);
 
     return (
-        <h1>Planilla de pago</h1>
+            loading
+            ?
+            <Spinner />
+            :
+            <div className="card shadow rounded">
+                <div className="card-header text-dark">
+                    Clientes
+                </div>
+                <div className="card-body">
+                    <div className="row mt-2 ml-1">
+                        <div className="col">
+                            <button className='btn btn-sm btn-primary' data-toggle="modal" data-target='#addPayForm'><i className="fas fa-plus mr-2"></i>Agregar</button>
+                        </div>
+                    </div>
+                    <DataTable
+                        columns={columns}
+                        data={filteredItems}
+                        responsive
+                        pagination
+                        paginationComponentOptions={paginationComponentOptions}
+                        subHeader
+                        subHeaderComponent={subHeaderComponentMemo}
+                        highlightOnHover
+                        striped
+                        persistTableHead 
+                    />
+
+                    <Modal 
+                        idModal='addPayForm'
+                        title='Agregar Planilla'
+                        messageError={messageError}
+                        content={<AddPayForm />}
+                    />
+                </div>
+            </div> 
     )
 }
 
