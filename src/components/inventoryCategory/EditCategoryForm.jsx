61a2de04ae4fe7f8f1286e55743d816178077f42
Mendel Aguilar <mendelaguilar1999123@gmail.com>
2022-08-21 00:47:35 -0600

    Actualizar

--- src/components/inventoryCategory/EditCategoryForm.jsx
+++ src/components/inventoryCategory/EditCategoryForm.jsx
@@ -1,34 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from '../../config/axios';
 import token from '../../helpers/getToken';
 import { toUpperCase } from "../../helpers/Mayusculas";
 
-const EditCategoryForm = ({rowCOD, setSendRequest, setMessageError}) => {
+const EditCategoryForm = ({setSendRequest, setMessageError}) => {
 
-    const [formEditCategor, setFormEditCategor] = useState({
+    const [formAddCategor, setFormAddCategor] = useState({
         NAM_CATEGORY: '',
-        DESCRIPTION: ''
+        DESCRIPTION:'',
+        COD_STATUS: ''
     })
 
     const handleInputChange = (e) => {
-        setFormEditCategor({
-            ...formEditCategor,
+        setFormAddCategor({
+            ...formAddCategor,
             [e.target.name]: e.target.value
         })
     }
 
-    useEffect(() => {
-        if(rowCOD){
-            axios.get(`/inventoryCategory/${rowCOD}`, token())
-            .then(res => setFormEditCategor(res.data[0]))
-        }
-    }, [rowCOD])
-
     const handleSubmitCategor = (e) => {
         e.preventDefault();
-        axios.put(`/inventoryCategory/${rowCOD}`,formEditCategor, token())
+        axios.post('/inventoryCategory', formAddCategor, token())
             .then(res => {
-                document.querySelector('#idCloseEditForm').click();
+                document.querySelector('#idCloseAddForm').click();
+                e.target.reset();
                 setSendRequest(true);
             })
             .catch(err => {
@@ -42,21 +37,28 @@ const EditCategoryForm = ({rowCOD, setSendRequest, setMessageError}) => {
     }
 
     return(
-        <form id='editFormCategory' onSubmit={handleSubmitCategor} action='#'>
+        <form id='addFormCategory' onSubmit={handleSubmitCategor} action='#'>
             <div className="row mb-4">
-            <div className="col-md-4">
-                    <label className='form-label' htmlFor="NAM_CATEGORY">Código producto</label>
-                    <input onChange={handleInputChange} value = {formEditCategor.NAM_CATEGORY} className='form-control' name='NAM_CATEGORY' type="text" onInput={toUpperCase} required/>
+                <div className="col-md-4">
+                    <label className='form-label' htmlFor="NAM_CATEGORY">Nombre categoría <span className="text-danger">  *</span></label>
+                    <input onChange={handleInputChange} className='form-control' name='NAM_CATEGORY' type="text" pattern="^[a-zA-ZñÑ ]+$"  title="No debe contener caracteres numericos" onInput={toUpperCase}required/>
                 </div>
                 <div className="col-md-4">
+                    <label className='form-label' htmlFor="COD_STATUS">Estado  <span className="text-danger"> *</span></label>
+                    <select onChange={handleInputChange} defaultValue={''} className='form-control' name='COD_STATUS' required>
+                        <option value=''>-Seleccionar-</option>
+                        <option value="1">Activo</option>
+                        <option value="2">Inactivo</option>
+                    </select>         
+                </div>
+                <div className="col-md-8 mt-2">
                     <label className='form-label' htmlFor="DESCRIPTION">Descripción</label>
-                    <input onChange={handleInputChange} value = {formEditCategor.DESCRIPTION} className='form-control' name='DESCRIPTION' type="text" onInput={toUpperCase} required/>
+                    <textarea onChange={handleInputChange} className='form-control' rows={3} name='DESCRIPTION' type="text" pattern="^[a-zA-Z0-9ñÑØº-_ ]+$" onInput={toUpperCase} wrap="hard"/>
                 </div>
             </div>
             <div className="modal-footer">
-                <button type="button" id='idCloseEditForm' className="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                <button type="button" id='idCloseAddForm' className="btn btn-primary" data-dismiss="modal">Cerrar</button>
                 <button type='submit' className="btn btn-success">Guardar</button>
-               
             </div>
         </form>
     )
